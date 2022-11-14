95c470b3f71c582c46c1a9e3b17fa4d8732883b7
gkvignesh5 <96396354+gkvignesh5@users.noreply.github.com>
2022-11-14 18:52:23 -0500

    Create data.js

---
+++ test/data.js
@@ -0,0 +1,8 @@
+module.exports.data = [
+    { instrument: 'Instrument', price: 18.5, quantity: 15, side: 'BUY', type: 'ADD' },
+    { instrument: 'Instrument', price: 10.1, quantity: 5, side: 'BUY', type: 'ADD' },
+    { instrument: 'Instrument', price: 17.6, quantity: 2, side: 'BUY', type: 'ADD' },
+    { instrument: 'Instrument', price: 28.5, quantity: 7, side: 'SELL', type: 'ADD' },
+    { instrument: 'Instrument', price: 17, quantity: 15, side: 'BUY', type: 'ADD' },
+    { instrument: 'Instrument', price: 18.5, quantity: 15, side: 'BUY', type: 'CANCEL' },
+]
